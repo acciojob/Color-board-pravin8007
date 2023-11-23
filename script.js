c43212a3c75c12784da03a2b66fc64b64a9f2675
Pravin Patil <106026547+pravin8007@users.noreply.github.com>
2023-11-23 09:36:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your JS code here. If required.
+ document.addEventListener('DOMContentLoaded', function () {
+      const container = document.getElementById('color-board');
+
+      // Create 800 boxes dynamically
+      for (let i = 0; i < 800; i++) {
+        const square = document.createElement('div');
+        square.className = 'square';
+        container.appendChild(square);
+      }
+
+      // Add event listeners for hover and transition
+      const squares = document.querySelectorAll('.square');
+      squares.forEach((square) => {
+        square.addEventListener('mouseover', function () {
+          this.style.backgroundColor = 'royalblue';
+        });
+
+        square.addEventListener('mouseout', function () {
+          setTimeout(() => {
+            this.style.backgroundColor = 'lightgray';
+          }, 1000);
+        });
+      });
+    });
